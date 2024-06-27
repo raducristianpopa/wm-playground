58db70bb4715537886f4419075b9066e973126e2
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2024-06-27 13:41:43 +0300

    Playground & Welcome POC

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="astro/client" />
+
+declare global {
+    interface MonetizationEvent extends Event {
+        readonly paymentPointer: string;
+        readonly incomingPayment: string;
+        readonly amount: PaymentCurrencyAmount;
+    }
+
+    interface ElementEventMap {
+        monetization: MonetizationEvent;
+    }
+}
+
+export {};
